e13ea904c50a85344a0a6446ee4bfd9662075fac
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2025-04-28 11:28:19 +0200

    feat(hestia_bme2080):
    Method now

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ logger = setup_logger(__name__)
 # importent that moduels are imported after the logger is set up. 
 from modules.camera.hestia_camera import capture_jpeg_binary
 
+# Temperature, Humidity and Pressure sensor import
+from modules.m_bme2080 import hestia_bme2080 as bme280
+
 def main():
     logger.info('Started')
 
     capture_jpeg_binary(640, 480)
         
     logger.info('Finished')
+
+    bme280_output = bme280.bme280_module()
 
 if __name__ == '__main__':
     main()
--- modules/m_bme2080/hestia_bme2080.py
+++ modules/m_bme2080/hestia_bme2080.py
@@ -1,6 +1,5 @@
 import smbus2
 import bme280
-import time
 import json
 
 port = 1
@@ -11,16 +10,22 @@ bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
 
-while True:
+# Function that takes the measurements from the sensor,
+# formats it as a json and returns it
+def bme280_module():
 	data = bme280.sample(bus, address, calibration_params)
 
-	bme = { "bme":[
-		{"Temperature":data.temperature,
-		"Humidity":data.humidity,
-		"Pressure":data.pressure
-		}]
+	bme = {"bme": [
+		{"Temperature": data.temperature,
+		 "Humidity": data.humidity,
+		 "Pressure": data.pressure
+		 }]
 	}
 
-	print (json.dumps(bme))
+	bme280_output = json.dumps(bme)
 
-	time.sleep(2)
+	print(bme280_output)
+
+	return bme280_output
+
+
